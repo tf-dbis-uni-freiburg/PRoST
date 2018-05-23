d7feb1e7fb82f77f4fbca3d11390a94b3873427f
GEMMA\arrascue <arrascue@informatik.uni-freiburg.de>
2018-05-23 18:22:07 +0200

    Solving some bugs, like entries with nulls and columns in Hive with the same name.

--- loader/src/main/java/loader/PropertyTableLoader.java
+++ loader/src/main/java/loader/PropertyTableLoader.java
@@ -2,7 +2,12 @@ package loader;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
@@ -11,189 +16,236 @@ import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
 
-
 /**
- * Class that constructs complex property table. It operates over set of RDF triples, collects and transforms
- * information about them into a table. If we have  a list of predicates/properties p1, ... , pN, then
- * the scheme of the table is (s: STRING, p1: LIST<STRING> OR STRING, ..., pN: LIST<STRING> OR STRING).
- * Column s contains subjects. For each subject , there is only one row in the table. Each predicate can be
- * of complex or simple type. If a predicate is of simple type means that there is no subject which has more
- * than one triple containing this property/predicate. Then the predicate column is of type STRING. Otherwise,
- * if a predicate is of complex type which means that there exists at least one subject which has more
- * than one triple containing this property/predicate. Then the predicate column is of type LIST<STRING>.
+ * Class that constructs complex property table. It operates over set of RDF
+ * triples, collects and transforms information about them into a table. If we
+ * have a list of predicates/properties p1, ... , pN, then the scheme of the
+ * table is (s: STRING, p1: LIST<STRING> OR STRING, ..., pN: LIST<STRING> OR
+ * STRING). Column s contains subjects. For each subject , there is only one row
+ * in the table. Each predicate can be of complex or simple type. If a predicate
+ * is of simple type means that there is no subject which has more than one
+ * triple containing this property/predicate. Then the predicate column is of
+ * type STRING. Otherwise, if a predicate is of complex type which means that
+ * there exists at least one subject which has more than one triple containing
+ * this property/predicate. Then the predicate column is of type LIST<STRING>.
  *
  * @author Matteo Cossu
  */
 public class PropertyTableLoader extends Loader {
 
+	protected String hdfs_input_directory;
 
-    protected String hdfs_input_directory;
+	private String tablename_properties = "properties";
 
-    private String tablename_properties = "properties";
+	/**
+	 * Separator used internally to distinguish two values in the same string
+	 */
+	public String columns_separator = "\\$%";
 
-    /**
-     * Separator used internally to distinguish two values in the same string
+	protected String output_db_name;
+	protected static final String output_tablename = "property_table";
+
+	public PropertyTableLoader(String hdfs_input_directory, String database_name, SparkSession spark) {
+		super(hdfs_input_directory, database_name, spark);
+	}
+
+	public void load() {
+		logger.info("PHASE 2: creating the property table...");
+
+		buildProperties();
+
+		// collect information for all properties
+		List<Row> props = spark.sql(String.format("SELECT * FROM %s", tablename_properties)).collectAsList();
+		String[] allProperties = new String[props.size()];
+		Boolean[] isMultivaluedProperty = new Boolean[props.size()];
+
+		for (int i = 0; i < props.size(); i++) {
+			allProperties[i] = props.get(i).getString(0);
+			isMultivaluedProperty[i] = props.get(i).getInt(1) == 1;
+		}
+	
+		//We create a map with the properties and the boolean.
+		Map<String, Boolean> propertiesMultivaluesMap = new HashMap<String, Boolean>();
+		for (int i = 0; i < allProperties.length; i++) {
+			String property = allProperties[i];
+			Boolean multivalued = isMultivaluedProperty[i];
+			propertiesMultivaluesMap.put(property, multivalued);
+		}
+
+		Map<String, Boolean> fixedPropertiesMultivaluesMap 
+			= handleCaseInsPred(propertiesMultivaluesMap);
+		
+		List<String> allPropertiesList = new ArrayList<String>();
+		List<Boolean> isMultivaluedPropertyList = new ArrayList<Boolean>();
+		allPropertiesList.addAll(fixedPropertiesMultivaluesMap.keySet());
+		
+		for (int i = 0; i < allPropertiesList.size(); i++) {
+			String property = allPropertiesList.get(i);
+			isMultivaluedPropertyList.add(fixedPropertiesMultivaluesMap.get(property));
+		}
+		
+		logger.info("All properties as array: " + allPropertiesList);
+		logger.info("Multi-values flag as array: " + isMultivaluedPropertyList);
+		
+		allProperties = (String[]) allPropertiesList.toArray();
+		this.properties_names = allProperties;
+		isMultivaluedProperty = (Boolean[]) isMultivaluedPropertyList.toArray();
+
+		// create multivalued property table
+		buildMultivaluedPropertyTable(allProperties, isMultivaluedProperty);
+	}
+
+	/**
+     * This method handles the problem when two predicate are the same in a 
+     * case-insensitive context but different in a case-sensitve one. For instance: 
+     * <http://example.org/somename> and <http://example.org/someName>.
+     *  Since Hive is case insensitive the problem will be solved removing one of 
+     *  the entries from the list of predicates.
      */
-    public String columns_separator = "\\$%";
-
-    protected String output_db_name;
-    protected static final String output_tablename = "property_table";
-
-    public PropertyTableLoader(String hdfs_input_directory,
-                               String database_name, SparkSession spark) {
-        super(hdfs_input_directory, database_name, spark);
+    public Map<String, Boolean> handleCaseInsPred(Map<String, Boolean> propertiesMultivaluesMap) {
+    		Set<String> seenPredicates = new HashSet<String>();
+    		Set<String> originalRemovedPredicates = new HashSet<String>(); 
+    		
+    		Iterator it = propertiesMultivaluesMap.keySet().iterator();
+    	    while (it.hasNext()) {
+    	    	String predicate = (String)it.next();
+    	    	if (seenPredicates.contains(predicate.toLowerCase())){
+    	    		propertiesMultivaluesMap.remove(predicate);
+    	    		originalRemovedPredicates.add(predicate); 
+    	    	}
+    	    	else{
+    	    		seenPredicates.add(predicate.toLowerCase());
+    	    	}    	    		
+    	    }    	    
+    	    if (originalRemovedPredicates.size() > 0)
+    	    	logger.info("The following predicates had to be removed from the list of predicates: " + originalRemovedPredicates);    				
+    	    return propertiesMultivaluesMap;
     }
 
-    public void load() {
-    	logger.info("PHASE 2: creating the property table...");
+	public void buildProperties() {
+		// return rows of format <predicate, is_complex>
+		// is_complex can be 1 or 0
+		// 1 for multivalued predicate, 0 for single predicate
 
-        buildProperties();
+		// select the properties that are multivalued
+		Dataset<Row> multivaluedProperties = spark.sql(String.format(
+				"SELECT DISTINCT(%1$s) AS %1$s FROM "
+						+ "(SELECT %2$s, %1$s, COUNT(*) AS rc FROM %3$s GROUP BY %2$s, %1$s HAVING rc > 1) AS grouped",
+				column_name_predicate, column_name_subject, name_tripletable));
 
-        // collect information for all properties
-        List<Row> props = spark.sql(String.format("SELECT * FROM %s", tablename_properties)).collectAsList();
-        String[] allProperties = new String[props.size()];
-        Boolean[] isMultivaluedProperty = new Boolean[props.size()];
+		List<String> multivaluedPropertiesList = multivaluedProperties.as(Encoders.STRING()).collectAsList();
+		if (multivaluedPropertiesList.size() > 0) {
+			logger.info("Multivalued Properties found:");
+			for (String property : multivaluedPropertiesList) {
+				logger.info(property);
+			}
+		}
 
-        for (int i = 0; i < props.size(); i++) {
-            allProperties[i] = props.get(i).getString(0);
-            isMultivaluedProperty[i] = props.get(i).getInt(1) == 1;
-        }
-        this.properties_names = allProperties;
-        
-        List<String> allPropertiesList = Arrays.asList(allProperties);
-        List<Boolean> isMultivaluedPropertyList = Arrays.asList(isMultivaluedProperty);
-        
-        logger.info("All properties as array: " + allPropertiesList);
-        logger.info("Multi-values flag as array: " + isMultivaluedPropertyList);
-        
-        // create multivalued property table               
-        buildMultivaluedPropertyTable(allProperties, isMultivaluedProperty);
-    }
+		// select all the properties
+		Dataset<Row> allProperties = spark
+				.sql(String.format("SELECT DISTINCT(%1$s) AS %1$s FROM %2$s", column_name_predicate, name_tripletable));
 
+		List<String> allPropertiesList = allProperties.as(Encoders.STRING()).collectAsList();
+		if (allPropertiesList.size() > 0) {
+			logger.info("All Properties found:");
+			for (String property : allPropertiesList) {
+				logger.info(property);
+			}
+		}
 
-    public void buildProperties() {
-        // return rows of format <predicate, is_complex>
-        // is_complex can be 1 or 0
-        // 1 for multivalued predicate, 0 for single predicate
+		// select the properties that are not multivalued
+		Dataset<Row> singledValueProperties = allProperties.except(multivaluedProperties);
 
-        // select the properties that are multivalued
-        Dataset<Row> multivaluedProperties = spark.sql(String.format(
-                "SELECT DISTINCT(%1$s) AS %1$s FROM "
-                        + "(SELECT %2$s, %1$s, COUNT(*) AS rc FROM %3$s GROUP BY %2$s, %1$s HAVING rc > 1) AS grouped",
-                column_name_predicate, column_name_subject, name_tripletable));
-        
-        List<String> multivaluedPropertiesList = multivaluedProperties.as(Encoders.STRING()).collectAsList();
-        if (multivaluedPropertiesList.size() > 0) {
-            logger.info("Multivalued Properties found:");
-            for (String property: multivaluedPropertiesList) {
-            	logger.info(property);
-            }
-        }        
+		List<String> singledValuePropertiesList = singledValueProperties.as(Encoders.STRING()).collectAsList();
+		if (singledValuePropertiesList.size() > 0) {
+			logger.info("Single-valued Properties found:");
+			for (String property : singledValuePropertiesList) {
+				logger.info(property);
+			}
+		}
 
-        // select all the properties
-        Dataset<Row> allProperties = spark.sql(String.format("SELECT DISTINCT(%1$s) AS %1$s FROM %2$s",
-                column_name_predicate, name_tripletable));
-        
-        List<String> allPropertiesList = allProperties.as(Encoders.STRING()).collectAsList();
-        if (allPropertiesList.size() > 0) {
-            logger.info("All Properties found:");
-            for (String property: allPropertiesList) {
-            	logger.info(property);
-            }
-        }
-        
-        // select the properties that are not multivalued
-        Dataset<Row> singledValueProperties = allProperties.except(multivaluedProperties);
-        
-        List<String> singledValuePropertiesList  = singledValueProperties.as(Encoders.STRING()).collectAsList();
-        if (singledValuePropertiesList.size() > 0) {
-            logger.info("Single-valued Properties found:");
-            for (String property: singledValuePropertiesList) {
-            	logger.info(property);
-            }
-        }
+		// combine them
+		Dataset<Row> combinedProperties = singledValueProperties.selectExpr(column_name_predicate, "0 AS is_complex")
+				.union(multivaluedProperties.selectExpr(column_name_predicate, "1 AS is_complex"));
 
-        // combine them
-        Dataset<Row> combinedProperties = singledValueProperties
-                .selectExpr(column_name_predicate, "0 AS is_complex")
-                .union(multivaluedProperties.selectExpr(column_name_predicate, "1 AS is_complex"));        
-        
-        List combinedPropertiesList = combinedProperties.as(Encoders.tuple(Encoders.STRING(), Encoders.INT())).collectAsList();
-        if (combinedPropertiesList.size() > 0) {
-            logger.info("All Properties with a flag to specify whether they are multi-valued:");
-            for (Object property: combinedPropertiesList) {
-            	logger.info(property);
-            }
-        }
+		List combinedPropertiesList = combinedProperties.as(Encoders.tuple(Encoders.STRING(), Encoders.INT()))
+				.collectAsList();
+		if (combinedPropertiesList.size() > 0) {
+			logger.info("All Properties with a flag to specify whether they are multi-valued:");
+			for (Object property : combinedPropertiesList) {
+				logger.info(property);
+			}
+		}
 
-        // remove '<' and '>', convert the characters
-        Dataset<Row> cleanedProperties = combinedProperties.withColumn("p",
-                functions.regexp_replace(functions.translate(combinedProperties.col("p"), "<>", ""),
-                        "[[^\\w]+]", "_"));
+		// remove '<' and '>', convert the characters
+		Dataset<Row> cleanedProperties = combinedProperties.withColumn("p",
+				functions.regexp_replace(functions.translate(combinedProperties.col("p"), "<>", ""), "[[^\\w]+]", "_"));
 
-        List cleanedPropertiesList = cleanedProperties.as(Encoders.tuple(Encoders.STRING(), Encoders.INT())).collectAsList();
-        if (cleanedPropertiesList.size() > 0) {
-            logger.info("Clean Properties (stored):");
-            for (Object property: cleanedPropertiesList) {
-            	logger.info(property);
-            }
-        } 
-        
-        // write the result
-        cleanedProperties.write().mode(SaveMode.Overwrite).saveAsTable("properties");
-        logger.info("Created properties table with name: " + tablename_properties);
-    }
+		List cleanedPropertiesList = cleanedProperties.as(Encoders.tuple(Encoders.STRING(), Encoders.INT()))
+				.collectAsList();
+		if (cleanedPropertiesList.size() > 0) {
+			logger.info("Clean Properties (stored):");
+			for (Object property : cleanedPropertiesList) {
+				logger.info(property);
+			}
+		}
 
-    /**
-     * Create the final property table, allProperties contains the list of all
-     * possible properties isMultivaluedProperty contains (in the same order used by
-     * allProperties) the boolean value that indicates if that property is
-     * multi-valued or not.
-     */
-    public void buildMultivaluedPropertyTable(String[] allProperties, Boolean[] isMultivaluedProperty) {
-    	logger.info("Building the complete property table.");
+		// write the result
+		cleanedProperties.write().mode(SaveMode.Overwrite).saveAsTable("properties");
+		logger.info("Created properties table with name: " + tablename_properties);
+	}
 
-        // create a new aggregation environment
-        PropertiesAggregateFunction aggregator = new PropertiesAggregateFunction(allProperties, columns_separator);
+	/**
+	 * Create the final property table, allProperties contains the list of all
+	 * possible properties isMultivaluedProperty contains (in the same order
+	 * used by allProperties) the boolean value that indicates if that property
+	 * is multi-valued or not.
+	 */
+	public void buildMultivaluedPropertyTable(String[] allProperties, Boolean[] isMultivaluedProperty) {
+		logger.info("Building the complete property table.");
 
-        String predicateObjectColumn = "po";
-        String groupColumn = "group";
+		// create a new aggregation environment
+		PropertiesAggregateFunction aggregator = new PropertiesAggregateFunction(allProperties, columns_separator);
 
-        // get the compressed table
-        Dataset<Row> compressedTriples = spark.sql(String.format("SELECT %s, CONCAT(%s, '%s', %s) AS po FROM %s",
-                column_name_subject, column_name_predicate, columns_separator, column_name_object, name_tripletable));
+		String predicateObjectColumn = "po";
+		String groupColumn = "group";
 
-        // group by the subject and get all the data
-        Dataset<Row> grouped = compressedTriples.groupBy(column_name_subject)
-                .agg(aggregator.apply(compressedTriples.col(predicateObjectColumn)).alias(groupColumn));
+		// get the compressed table
+		Dataset<Row> compressedTriples = spark.sql(String.format("SELECT %s, CONCAT(%s, '%s', %s) AS po FROM %s",
+				column_name_subject, column_name_predicate, columns_separator, column_name_object, name_tripletable));
 
-        // build the query to extract the property from the array
-        String[] selectProperties = new String[allProperties.length + 1];
-        selectProperties[0] = column_name_subject;
-        for (int i = 0; i < allProperties.length; i++) {
+		// group by the subject and get all the data
+		Dataset<Row> grouped = compressedTriples.groupBy(column_name_subject)
+				.agg(aggregator.apply(compressedTriples.col(predicateObjectColumn)).alias(groupColumn));
 
-            // if property is a full URI, remove the < at the beginning end > at the end
-            String rawProperty = allProperties[i].startsWith("<") && allProperties[i].endsWith(">") ?
-                    allProperties[i].substring(1, allProperties[i].length() - 1) : allProperties[i];
-            // if is not a complex type, extract the value
-            String newProperty = isMultivaluedProperty[i]
-                    ? " " + groupColumn + "[" + String.valueOf(i) + "] AS " + getValidHiveName(rawProperty)
-                    : " " + groupColumn + "[" + String.valueOf(i) + "][0] AS " + getValidHiveName(rawProperty);
-            selectProperties[i + 1] = newProperty;
-        }
-        
-        List<String> allPropertiesList = Arrays.asList(selectProperties);
-        logger.info("Columns of  Property Table: " + allPropertiesList);
-        
-        Dataset<Row> propertyTable = grouped.selectExpr(selectProperties);
-        
-        Row sampledRow = propertyTable.first();
-        logger.info("****" + sampledRow);    
+		// build the query to extract the property from the array
+		String[] selectProperties = new String[allProperties.length + 1];
+		selectProperties[0] = column_name_subject;
+		for (int i = 0; i < allProperties.length; i++) {
 
-        // write the final one, partitioned by subject
-        propertyTable.write().mode(SaveMode.Overwrite).format(table_format).saveAsTable(output_tablename);
-        logger.info("Created property table with name: " + output_tablename);
+			// if property is a full URI, remove the < at the beginning end > at
+			// the end
+			String rawProperty = allProperties[i].startsWith("<") && allProperties[i].endsWith(">")
+					? allProperties[i].substring(1, allProperties[i].length() - 1) : allProperties[i];
+			// if is not a complex type, extract the value
+			String newProperty = isMultivaluedProperty[i]
+					? " " + groupColumn + "[" + String.valueOf(i) + "] AS " + getValidHiveName(rawProperty)
+					: " " + groupColumn + "[" + String.valueOf(i) + "][0] AS " + getValidHiveName(rawProperty);
+			selectProperties[i + 1] = newProperty;
+		}
 
-    }
+		List<String> allPropertiesList = Arrays.asList(selectProperties);
+		logger.info("Columns of  Property Table: " + allPropertiesList);
+
+		Dataset<Row> propertyTable = grouped.selectExpr(selectProperties);
+
+		Row sampledRow = propertyTable.first();
+		logger.info("****" + sampledRow);
+
+		// write the final one, partitioned by subject
+		propertyTable.write().mode(SaveMode.Overwrite).format(table_format).saveAsTable(output_tablename);
+		logger.info("Created property table with name: " + output_tablename);
+
+	}
 
 }
--- loader/src/main/java/loader/TripleTableLoader.java
+++ loader/src/main/java/loader/TripleTableLoader.java
@@ -5,70 +5,91 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
 
-
 /**
- * Class that constructs the triple table. It is created as external table, so it can use the input file
- * directly without loosing time to replicate the data, since the triple table will be used only for other
- * models e.g. Property Table, Vertical Partitioning.
+ * Class that constructs the triple table. It is created as external table, so
+ * it can use the input file directly without loosing time to replicate the
+ * data, since the triple table will be used only for other models e.g. Property
+ * Table, Vertical Partitioning.
  *
  * @author Matteo Cossu
+ * @author Victor Anthony Arrascue Ayala
  */
 public class TripleTableLoader extends Loader {
 
-    public TripleTableLoader(String hdfs_input_directory, String database_name,
-                             SparkSession spark) {
-        super(hdfs_input_directory, database_name, spark);
-    }
+	public TripleTableLoader(String hdfs_input_directory, String database_name, SparkSession spark) {
+		super(hdfs_input_directory, database_name, spark);
+	}
 
-    @Override
-    public void load() {
-    	logger.info("PHASE 1: loading all triples to a generic table...");
-        String createTripleTable = String.format(
-                "CREATE EXTERNAL TABLE IF NOT EXISTS %s(%s STRING, %s STRING, %s STRING) ROW FORMAT  SERDE"
-                        + "'org.apache.hadoop.hive.serde2.RegexSerDe'  WITH SERDEPROPERTIES "
-                        + "( \"input.regex\" = \"(\\\\S+)\\\\s+(\\\\S+)\\\\s+(.*)\")"
-                        + "LOCATION '%s'",
-                name_tripletable, column_name_subject, column_name_predicate, column_name_object, hdfs_input_directory);
+	@Override
+	public void load() {
+		logger.info("PHASE 1: loading all triples to a generic table...");
+		String createTripleTable = String.format(
+				"CREATE EXTERNAL TABLE IF NOT EXISTS %s(%s STRING, %s STRING, %s STRING) ROW FORMAT  SERDE"
+						+ "'org.apache.hadoop.hive.serde2.RegexSerDe'  WITH SERDEPROPERTIES "
+						+ "( \"input.regex\" = \"(\\\\S+)\\\\s+(\\\\S+)\\\\s+(.*)\")" + "LOCATION '%s'",
+				name_tripletable, column_name_subject, column_name_predicate, column_name_object, hdfs_input_directory);
 
-        spark.sql(createTripleTable);
-        logger.info("Created tripletable with " + createTripleTable);
-        checkPropertiesOfTripleTable();
-    }
-    
-    public void checkPropertiesOfTripleTable() {
-    	logger.info("DESCRIBE EXTENDED: " + spark.sql("DESCRIBE EXTENDED " + name_tripletable));
-    	long numLoadedTriples = spark.sql(String.format("SELECT * FROM %s", name_tripletable)).count();
-    	logger.info("Number of raw triples loaded: " + numLoadedTriples);
-    	String queryFailedSubjects = String.format("SELECT * FROM %s WHERE %s IS NULL", name_tripletable, column_name_subject);
-    	long numFailedSubjects = spark.sql(queryFailedSubjects).count();
-    	logger.info("Number of lines in which subject is null: " + numFailedSubjects);
-    	String queryFailedPredicates = String.format("SELECT * FROM %s WHERE %s IS NULL", name_tripletable, column_name_predicate);
-    	long numFailedPredicates = spark.sql(queryFailedPredicates).count();
-    	logger.info("Number of lines in which predicate is null: " + numFailedPredicates);
-    	String queryFailedObjects = String.format("SELECT * FROM %s WHERE %s IS NULL", name_tripletable, column_name_object);
-    	long numFailedObjects = spark.sql(queryFailedObjects).count();
-    	logger.info("Number of lines in which predicate is null: " + numFailedObjects);
-    	if ((numFailedSubjects + numFailedPredicates + numFailedObjects) >0 ) {
-    		logger.error("This dataset has some problems: " + numFailedObjects);
-    		correctTripleTables();
-    	}	
-    }
-    
-    //TODO
-    public void correctTripleTables(){
-    	logger.info("The table could not upload all triples from HDFS. Attempting to remove corrupted triples.");
-    }
+		spark.sql(createTripleTable);
+		logger.info("Created tripletable with " + createTripleTable);
+		checkPropertiesOfTripleTable();
+		deleteDotsFromObjects();
+	}
 
-    // this method exists for the sake of clarity instead of a constant String
-    // Therefore, it should be called only once
-    private static String build_triple_regex() {
-        String uri_s = "<(?:[^:]+:[^\\s\"<>]+)>";
-        String literal_s = "\"(?:[^\"\\\\]*(?:\\.[^\"\\\\]*)*)\"(?:@([a-z]+(?:-[a-zA-Z0-9]+)*)|\\^\\^" + uri_s + ")?";
-        String subject_s = "(" + uri_s + "|" + literal_s + ")";
-        String predicate_s = "(" + uri_s + ")";
-        String object_s = "(" + uri_s + "|" + literal_s + ")";
-        String space_s = "[ \t]+";
-        return "[ \\t]*" + subject_s + space_s + predicate_s + space_s + object_s + "[ \\t]*\\.*[ \\t]*(#.*)?";
-    }
+	/**
+	 * Logs important information about the triples loaded.
+	 */
+	public void checkPropertiesOfTripleTable() {
+		logger.info("DESCRIBE EXTENDED: " + spark.sql("DESCRIBE EXTENDED " + name_tripletable));
+		long numLoadedTriples = spark.sql(String.format("SELECT * FROM %s", name_tripletable)).count();
+		logger.info("Number of raw triples loaded: " + numLoadedTriples);
+		String queryFailedSubjects = String.format("SELECT * FROM %s WHERE %s IS NULL", name_tripletable,
+				column_name_subject);
+		long numFailedSubjects = spark.sql(queryFailedSubjects).count();
+		logger.info("Number of lines in which subject is null: " + numFailedSubjects);
+		String queryFailedPredicates = String.format("SELECT * FROM %s WHERE %s IS NULL", name_tripletable,
+				column_name_predicate);
+		long numFailedPredicates = spark.sql(queryFailedPredicates).count();
+		logger.info("Number of lines in which predicate is null: " + numFailedPredicates);
+		String queryFailedObjects = String.format("SELECT * FROM %s WHERE %s IS NULL", name_tripletable,
+				column_name_object);
+		long numFailedObjects = spark.sql(queryFailedObjects).count();
+		logger.info("Number of lines in which predicate is null: " + numFailedObjects);
+		if ((numFailedSubjects + numFailedPredicates + numFailedObjects) > 0) {
+			logger.error("This dataset has some problems: " + numFailedObjects);
+			correctTripleTables();
+		}
+	}
+
+	/**
+	 * If the file contains triples of the form (<s> <p>  <o> .)
+	 * with the dot at the end then object is uploaded. This is the case
+	 * for literals. This method corrects this.
+	 */
+	public void deleteDotsFromObjects() {
+		
+	}
+
+	/**
+	 * Removes rows containing nulls.
+	 */
+	public void correctTripleTables() {
+		logger.info("The table could not upload all triples from HDFS. Attempting to remove corrupted triples.");
+		String.format("DELETE FROM %s WHERE %s IS NULL or %s IS NULL or %s is NULL", name_tripletable,
+				column_name_subject, column_name_predicate, column_name_object);
+		long numLoadedTriples = spark.sql(String.format("SELECT * FROM %s", name_tripletable)).count();
+		logger.info("Number of raw triples loaded: " + numLoadedTriples);
+	}
+
+	// this method exists for the sake of clarity instead of a constant String
+	// Therefore, it should be called only once
+	private static String build_triple_regex() {
+		String uri_s = "<(?:[^:]+:[^\\s\"<>]+)>";
+		String literal_s = "\"(?:[^\"\\\\]*(?:\\.[^\"\\\\]*)*)\"(?:@([a-z]+(?:-[a-zA-Z0-9]+)*)|\\^\\^" + uri_s + ")?";
+		String subject_s = "(" + uri_s + "|" + literal_s + ")";
+		String predicate_s = "(" + uri_s + ")";
+		String object_s = "(" + uri_s + "|" + literal_s + ")";
+		String space_s = "[ \t]+";
+		return "[ \\t]*" + subject_s + space_s + predicate_s + space_s + object_s + "[ \\t]*\\.*[ \\t]*(#.*)?";
+	}
 
 }
