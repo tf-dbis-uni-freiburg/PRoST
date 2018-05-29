2a2887d41077942bb078b167931d36ff6500b582
GEMMA\arrascue <arrascue@informatik.uni-freiburg.de>
2018-05-29 17:07:25 +0200

    Changes in the code to implement the cleaning of the triple table using only HIVE capabilities.

--- loader/src/main/java/loader/TripleTableLoader.java
+++ loader/src/main/java/loader/TripleTableLoader.java
@@ -3,20 +3,16 @@ package loader;
 import java.util.List;
 
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
-import static org.apache.spark.sql.functions.regexp_replace;
-import static org.apache.spark.sql.functions.col;
 
 
 
 /**
- * Class that constructs the triple table. It is created as external table, so
- * it can use the input file directly without loosing time to replicate the
- * data, since the triple table will be used only for other models e.g. Property
- * Table, Vertical Partitioning.
+ * Class that constructs a triples table. First, the loader creates an external table ("raw").
+ * The data is read using SerDe capabilities and by means of a regular expresion.
+ * An additional table ("fixed") is created to make sure that  only valid triples are passed to the 
+ * next stages in which other models e.g. Property Table, or Vertical Partitioning are built.
  *
  * @author Matteo Cossu
  * @author Victor Anthony Arrascue Ayala
@@ -29,179 +25,51 @@ public class TripleTableLoader extends Loader {
 
 	@Override
 	public void load() throws Exception {
-		logger.info("PHASE 1: loading all triples to a generic table...");
-		String createTripleTable = String.format(
+		logger.info("PHASE 1: loading all triples to a generic table...");		
+		String queryDropTripleTable = String.format("DROP TABLE IF EXISTS %s", name_tripletable);
+		String queryDropTripleTableFixed = String.format("DROP TABLE IF EXISTS %s", name_tripletable+"_fixed");
+
+		spark.sql(queryDropTripleTable);
+		spark.sql(queryDropTripleTableFixed);
+		
+		String createTripleTableRaw = String.format(
 				"CREATE EXTERNAL TABLE IF NOT EXISTS %s(%s STRING, %s STRING, %s STRING) ROW FORMAT  SERDE"
 						+ "'org.apache.hadoop.hive.serde2.RegexSerDe'  WITH SERDEPROPERTIES "
-						+ "( \"input.regex\" = \"(\\\\S+)\\\\s+(\\\\S+)\\\\s+(.*)\")" + "LOCATION '%s'",
-				name_tripletable, column_name_subject, column_name_predicate, column_name_object, hdfs_input_directory);
-
-		spark.sql(createTripleTable);
-		logger.info("Created tripletable with " + createTripleTable);
-		repairCorruptedTriples();
-	}
-
-	/**
-	 * Searches for different kinds of corrupted triples and removes them.
-	 * @throws Exception 
-	 */
-	public void repairCorruptedTriples() throws Exception {
-		String queryDropTempTable = String.format("DROP TABLE IF EXISTS %s", name_tripletable+"_temp");
-		spark.sql(queryDropTempTable);
-		String queryDropFixedTable = String.format("DROP TABLE IF EXISTS %s", name_tripletable+"_fixed");
-		spark.sql(queryDropFixedTable);
+						+ "( \"input.regex\" = \"(\\\\S+)\\\\s+(\\\\S+)\\\\s+(.+)\\\\s*\\\\.\\\\s*$\")" + "LOCATION '%s'",
+				name_tripletable+"_raw", column_name_subject, column_name_predicate, column_name_object, hdfs_input_directory);
+		spark.sql(createTripleTableRaw);
 		
-		Dataset<Row> allTriples = null;
-		Dataset<Row> triplesWithNulls = null;
-		Dataset<Row> triplesWithDotRes = null;
-		Dataset<Row> objectsWithMultipleItems = null;
+		String createTripleTableFixed = String.format(
+				"CREATE TABLE  IF NOT EXISTS  %s(%s STRING, %s STRING, %s STRING)", 
+					name_tripletable+"_fixed", column_name_subject, column_name_predicate, column_name_object);
+		spark.sql(createTripleTableFixed);
 		
-		String queryAllTriples = String.format("SELECT * FROM %s", name_tripletable);
-		allTriples = spark.sql(queryAllTriples);
+		String repairTripleTableFixed = String.format(
+				"INSERT OVERWRITE TABLE %1$s SELECT %2$s, %3$s, trim(%4$s) "
+						+ "FROM %5$s "
+						+ "WHERE %2$s is not null AND %3$s is not null AND %4$s is not null AND "
+						+ "NOT(%2$s RLIKE '^\\s*\\.\\s*$')  AND NOT(%3$s RLIKE '^\\s*\\.\\s*$') AND NOT(%4$s RLIKE '^\\s*\\.\\s*$') AND "
+						+ "NOT(%4$s RLIKE '^\\s*<.*<.*>')  AND NOT(%4$s RLIKE '(?<!\\u005C\\u005C)\".*(?<!\\u005C\\u005C)\".*(?<!\\u005C\\u005C)\"') ",
+				name_tripletable+"_fixed", column_name_subject, column_name_predicate, column_name_object, name_tripletable+"_raw");
+		spark.sql(repairTripleTableFixed);
 		
-		long numLoadedTriples = spark.sql(String.format("SELECT * FROM %s", name_tripletable)).count();
-		logger.info("Number of raw triples loaded: " + numLoadedTriples);
-		String queryFailedSubjects = String.format("SELECT * FROM %s WHERE %s IS NULL", name_tripletable,
-			column_name_subject);
-		long numFailedSubjects = spark.sql(queryFailedSubjects).count();
-		logger.info("Number of lines in which subject is null: " + numFailedSubjects);
-		String queryFailedPredicates = String.format("SELECT * FROM %s WHERE %s IS NULL", name_tripletable,
-				column_name_predicate);
-		long numFailedPredicates = spark.sql(queryFailedPredicates).count();
-		logger.info("Number of lines in which predicate is null: " + numFailedPredicates);
-		String queryFailedObjects = String.format("SELECT * FROM %s WHERE %s IS NULL", name_tripletable,
-				column_name_object);
-		long numFailedObjects = spark.sql(queryFailedObjects).count();
-		logger.info("Number of lines in which object is null: " + numFailedObjects);
-			
-		if ((numFailedSubjects + numFailedPredicates + numFailedObjects) > 0) {
-			triplesWithNulls = identifyNullsInTriples();
-			List corruptedList = triplesWithNulls.limit(5).collectAsList();
-			logger.info("First 5 triples with nulls (less if there are less): " + corruptedList);			
-			allTriples = allTriples.filter(column_name_subject+" is not null AND "+
-					column_name_predicate+" is not null AND "+
-					column_name_object+" is not null");
-		}
-		
-		String querySubjectsAreDots = String.format("SELECT * FROM %s WHERE %s RLIKE '^\\s*\\.\\s*$' ", name_tripletable,
-				column_name_subject);	
-		long numSubjectsAreDots = spark.sql(querySubjectsAreDots).count();
-		logger.info("Number of lines in which subject is not a valid resource: " + numSubjectsAreDots);
-		String queryPredicatesAreDots = String.format("SELECT * FROM %s WHERE %s RLIKE '^\\s*\\.\\s*$' ", name_tripletable,
-				column_name_predicate);	
-		long numPredicatesAreDots = spark.sql(queryPredicatesAreDots).count();
-		logger.info("Number of lines in which predicate is not a valid resource: " + numPredicatesAreDots);
-		String queryObjectsAreDots = String.format("SELECT * FROM %s WHERE %s RLIKE '^\\s*\\.\\s*$' ", name_tripletable,
-				column_name_object);	
-		long numObjectsAreDots = spark.sql(queryObjectsAreDots).count();
-		logger.info("Number of lines in which object is not a valid resource/literal: " + numObjectsAreDots);
-		
-		if ((numSubjectsAreDots + numPredicatesAreDots + numObjectsAreDots) > 0) {
-			triplesWithDotRes = identifyDotsInTriples();
-			List corruptedList = triplesWithDotRes.limit(5).collectAsList();
-			logger.info("First 5 triples with unvalid triples (less if there are less): " + corruptedList);
-			allTriples = allTriples.filter("NOT("+column_name_subject+" rlike '^\\s*\\.\\s*$') AND "+
-					"NOT("+column_name_predicate+" rlike '^\\s*\\.\\s*$' )  AND "+
-					"NOT("+column_name_object+"  rlike '^\\s*\\.\\s*$' )" );
-		}
-		
-		String queryMultipleObjectItems = String.format("SELECT * FROM %s WHERE %s RLIKE '^\\s*<.*<.*>' "
-				+ "or %s RLIKE '(?<!\\u005C\\u005C)\".*(?<!\\u005C\\u005C)\".*(?<!\\u005C\\u005C)\"'", name_tripletable,
-				column_name_object, column_name_object);	
-		long numObjectsWithMultipleObjects = spark.sql(queryMultipleObjectItems).count();
-		
-		if (numObjectsWithMultipleObjects > 0) {
-			objectsWithMultipleItems = identifyMultipleObjects();
-			List corruptedList = objectsWithMultipleItems.limit(5).collectAsList();
-			logger.info("First 5 triples with multiple items as object (less if there are less): " + corruptedList);
-			allTriples = allTriples.filter("NOT("+column_name_object+" rlike '^\\s*<.*<.*>') AND "+
-					"NOT("+column_name_object+" rlike '(?<!\\u005C\\u005C)\".*(?<!\\u005C\\u005C)\".*(?<!\\u005C\\u005C)\" ')");
-		}
-		
-		//One last step is required. If the triples contain a final "."
-		//it is necessary to remove it remove from the table, because it will appear there.
-		allTriples = allTriples.withColumn(column_name_object+"_clean",  
-				regexp_replace(allTriples.col(column_name_object), "(\\s*\\.\\s*)+$", ""));
-		allTriples = allTriples.selectExpr(
-				column_name_subject, column_name_predicate, column_name_object+"_clean AS " + column_name_object);
-		
+		logger.info("Created tripletable with: " + createTripleTableRaw);
+		logger.info("Cleaned tripletable created with: " + repairTripleTableFixed);
+				
+		String queryAllTriples = String.format("SELECT * FROM %s", name_tripletable+"_fixed");
+		Dataset<Row>  allTriples = spark.sql(queryAllTriples);
 		if (allTriples.count()==0) {
 			logger.error("Either your HDFS path does not contain any files or no triples were accepted in the given format (nt)");
 			logger.error("The program will stop here.");
 			throw new Exception("Empty HDFS directory or empty files within.");
 		}
-		
 		List cleanedList = allTriples.limit(10).collectAsList();
 		logger.info("First 10 cleaned triples (less if there are less): " + cleanedList);
-		
-		/*
-		allTriples.createOrReplaceTempView(name_tripletable+"_temp");
-		//allTriples.createOrReplaceTempView(name_tripletable+"_fixed");
-		String queryCreateFixedTable = 
-				String.format("CREATE TABLE %s AS SELECT * FROM %s ", 
-						name_tripletable+"_fixed", name_tripletable+"_temp");
-		spark.sql(queryCreateFixedTable);
-		String queryDropOriginalTable = String.format("DROP TABLE IF EXISTS %s", name_tripletable);
-		spark.sql(queryDropOriginalTable);*/
-						
-		allTriples.repartition(col(column_name_subject), col(column_name_predicate))
-			.write().partitionBy(column_name_subject, column_name_predicate).mode(SaveMode.Overwrite)
-			.format(table_format).saveAsTable(name_tripletable+"_fixed");
-	}
-	
-	/**
-	 * Identifies rows containing nulls.
-	 */
-	public Dataset<Row> identifyNullsInTriples() {
-		//We identify out from the rowCollection columns which contain nulls.
-		String noNullsQuery = String.format("SELECT * FROM %s WHERE %s IS NULL or %s IS NULL or %s is NULL", name_tripletable, 
-				column_name_subject, column_name_predicate, column_name_object);
-		Dataset<Row> rowsWithNulls = spark.sql(noNullsQuery);						
-		logger.info("Number of triples containing nulls : " + rowsWithNulls.count());
-		return rowsWithNulls;
-	}
-
-	/**
-	 * Identifies triples which contain only a dot (.). 
-	 * This means the triple was not complete in the original file.
-	 * @return
-	 */
-	public Dataset<Row> identifyDotsInTriples() {
-		//We identify out from the rowCollection columns which contain dots.
-		String noDotsQuery = 
-				String.format("SELECT * FROM %s WHERE %s RLIKE '^\\s*\\.\\s*$' or %s RLIKE "
-						+ "'^\\s*\\.\\s*$' or %s RLIKE '^\\s*\\.\\s*$'", name_tripletable, 
-						column_name_subject, column_name_predicate, column_name_object);
-		Dataset<Row> rowsWithDots = spark.sql(noDotsQuery);					
-		logger.info("Number of invalid triples : " + rowsWithDots.count());
-		return rowsWithDots;
-	}
-	
-	/**
-	 * Identifies multiple resources or literals in the object column.
-	 * This means that a triple in the original file had more than three elements.
-	 * This method identifies multiple resources of the same kind, i.e. two or more 
-	 * resources or two or more literals. The case where these are mixed is not considered.
-	 * @return
-	 */
-	public Dataset<Row> identifyMultipleObjects() {
-		//We identify out from the rowCollection columns which contain dots.
-		String queryMultipleResources = 
-				String.format("SELECT * FROM %s WHERE %s RLIKE '^\\s*<.*<.*>'", name_tripletable, 
-						column_name_object);
-		Dataset<Row> rowsWithMultipleResources = spark.sql(queryMultipleResources);		
-		String queryMultipleLiterals = 
-				String.format("SELECT * FROM %s WHERE %s RLIKE '(?<!\\u005C\\u005C)\".*(?<!\\u005C\\u005C)\".*(?<!\\u005C\\u005C)\"'", name_tripletable, 
-						column_name_object);
-		Dataset<Row> rowsWithMultipleLiterals = spark.sql(queryMultipleLiterals);	
-		
-		Dataset<Row> rowsWithMultipleItems = rowsWithMultipleResources.union(rowsWithMultipleLiterals);								
-		logger.info("Number of triples containing more than one resource or literal in the object column : " + rowsWithMultipleItems.count());
-		return rowsWithMultipleItems;
 	}
 
 	// this method exists for the sake of clarity instead of a constant String
 	// Therefore, it should be called only once
+	/*
 	private static String build_triple_regex() {
 		String uri_s = "<(?:[^:]+:[^\\s\"<>]+)>";
 		String literal_s = "\"(?:[^\"\\\\]*(?:\\.[^\"\\\\]*)*)\"(?:@([a-z]+(?:-[a-zA-Z0-9]+)*)|\\^\\^" + uri_s + ")?";
@@ -210,6 +78,5 @@ public class TripleTableLoader extends Loader {
 		String object_s = "(" + uri_s + "|" + literal_s + ")";
 		String space_s = "[ \t]+";
 		return "[ \\t]*" + subject_s + space_s + predicate_s + space_s + object_s + "[ \\t]*\\.*[ \\t]*(#.*)?";
-	}
-
+	}*/
 }
--- loader/test_dataset/READ_ME.txt
+++ loader/test_dataset/READ_ME.txt
@@ -4,3 +4,36 @@ test_case3: contains empty lines in between triples at the beginning and at end 
 test_case4: to assess whether the dot at the end of each line is removed correctly and it does not appear in the PT or VP. At the same time, only the final dot should be removed (this is particularly important for literals).
 test_case5: two predicates can be equal in a case insensitive comparison, but distinct in a case sensitive comparison. Example: pred1/givenName and pred1/givenname. Hive's table and column names are case insentive. Therefore, one of this predicates is discarded in the PT and VP.
 test_case6: It contains an empty file (it only contains a dot, otherwise it cannot be uploaded to HDFS). This leads to empty tables. PRoST-loader should raise an exception.
+
+
+Expected Results:
+****test_case1  -> PASSED
+<http://dbpedia.org/resource/Anarchism1>,<http://dbpedia.org/property/pro1>,<http://dbpedia.org/resource/Template:Sisterlinks>
+<http://dbpedia.org/resource/Anarchism1>,<http://dbpedia.org/property/pro1>,<http://dbpedia.org/resource/Res3>
+<http://dbpedia.org/resource/Anarchism3>,<http://dbpedia.org/property/pro3>,"wow\" \" . \"ok\" hi"
+<http://dbpedia.org/resource/Anarchism4>,<http://dbpedia.org/property/pro2>,<http://dbpedia.org/resource/Res4>
+<http://dbpedia.org/resource/Anarchism2>,<http://dbpedia.org/property/pro3>,"wow hi"
+
+****test_case2  -> PASSED
+<http://dbpedia.org/resource/Anarchism1>,<http://dbpedia.org/property/pro1>,<http://dbpedia.org/resource/Template:Sisterlinks> 
+<http://dbpedia.org/resource/Anarchism5>,<http://dbpedia.org/property/pro3>,<http://dbpedia.org/resource/Res2>
+
+****test_case3  -> PASSED
+<http://dbpedia.org/resource/Anarchism1>,<http://dbpedia.org/property/pro1>,<http://dbpedia.org/resource/Template:Sisterlinks>
+<http://dbpedia.org/resource/Anarchism5>,<http://dbpedia.org/property/pro3>,<http://dbpedia.org/resource/Res2>
+
+****test_case4  -> PASSED
+<http://dbpedia.org/resource/Anarchism1>,<http://dbpedia.org/property/pro1>,<http://dbpedia.org/resource/Template:Sisterlinks>
+<http://dbpedia.org/resource/Anarchism5>,<http://dbpedia.org/property/pro3>,"one literal"
+<http://dbpedia.org/resource/Anarchism2>,<http://dbpedia.org/property/pro1>,<http://dbpedia.org/resource/Template:Sisterlinks>
+<http://dbpedia.org/resource/Anarchism3>,<http://dbpedia.org/property/pro3>,"This literal contains a dot . which should NOT be removed"
+<http://dbpedia.org/resource/Anarchism4>,<http://dbpedia.org/property/pro1>,<http://dbpedia.org/resource/Template:Sisterlinks>
+<http://dbpedia.org/resource/Anarchism6>,<http://dbpedia.org/property/pro3>,"one literal"^^<type1>
+
+****test_case5  -> PASSED
+<http://dbpedia.org/resource/Anarchism1>,<http://dbpedia.org/property/pro1/givenname>,<http://dbpedia.org/resource/Template:Sisterlinks>
+<http://dbpedia.org/resource/Anarchism1>,<http://dbpedia.org/property/pro1/givenname>,<http://dbpedia.org/resource/Res3>
+<http://dbpedia.org/resource/Anarchism5>,<http://dbpedia.org/property/pro1/givenName>,<http://dbpedia.org/resource/Res1>
+
+****test_case6  -> PASSED
+ERROR PRoST - Either your HDFS path does not contain any files or no triples were accepted in the given format (nt)
