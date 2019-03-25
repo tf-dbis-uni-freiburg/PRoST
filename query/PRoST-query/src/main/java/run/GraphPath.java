1bf8996f75edca4a1c5fddce3cdbab8d2305eb6a
gschievelbein <gschievelbein@inf.ufrgs.br>
2019-03-25 19:25:10 +0100

    added methods to (re)partition wpt

--- query/PRoST-query/src/main/java/run/GraphPath.java
+++ query/PRoST-query/src/main/java/run/GraphPath.java
@@ -2,10 +2,7 @@ package run;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 import translator.Stats;
 
 import java.io.IOException;
@@ -17,20 +14,19 @@ public class GraphPath {
 
     private static final Logger logger = Logger.getLogger("PRoST");
 
-
     public static void main(final String[] args) throws IOException {
         final InputStream inStream = Main.class.getClassLoader().getResourceAsStream("log4j.properties");
         final Properties props = new Properties();
         props.load(inStream);
         PropertyConfigurator.configure(props);
 
-        String path = "r0   p0  r1";
-        String databaseName = "mag";
-        String statsFileName = "mag.stats";
-        String outputFile = "mag_paths";
+        final String path = "r0 p0  r1";
+        final String databaseName = "mag";
+        final String statsFileName = "mag.stats";
+        final String outputFile = "mag_paths";
+        final String sourcesOutputFile = "sources";
 
         Stats.getInstance().parseStats(statsFileName);
-
 
         SparkSession spark;
         SQLContext sqlContext;
@@ -41,18 +37,18 @@ public class GraphPath {
 
         //get source
 
-        String source = "r1";
+        String source = "<http://ma-graph.org/entity/100000009>";
 
-        Dataset<Row> paths = computeHops(sqlContext,source);
+        //Dataset<Row> paths = computeHops(sqlContext,source);
 
-        paths.write().parquet(outputFile);
+        //paths.write().parquet(outputFile);
+
+        //computeSources(sqlContext).write().parquet(sourcesOutputFile);
+
+        partitionTable(sqlContext, "wide_property_table", "wide_property_table_p", "s");
     }
 
     private static Dataset<Row> computeHops(final SQLContext sqlContext, String source) {
-        final ArrayList<String> selectVariables = new ArrayList<>();
-        final ArrayList<String> whereConditions = new ArrayList<>();
-        final ArrayList<String> explodedColumns = new ArrayList<>();
-
         Dataset<Row> result = null;
 
         logger.info("COMPUTING HOPS FROM " + source);
@@ -60,16 +56,24 @@ public class GraphPath {
         for (String column : Stats.getInstance().getTableNames()) {
             boolean isComplex = Stats.getInstance().getTableSize(column) != Stats.getInstance().getTableDistinctSubjects(column);
 
+            final ArrayList<String> selectVariables = new ArrayList<>();
+            final ArrayList<String> whereConditions = new ArrayList<>();
+            final ArrayList<String> explodedColumns = new ArrayList<>();
+
             //s= constant
             whereConditions.add("s='" + source + "'");
 
             //p= variable
-            selectVariables.add(column + " as p ");
+
+            //TODO change column to the String instead of array of Strings
+            selectVariables.add("'"+source+"' as s ");
+            selectVariables.add("'"+column+"' as p ");
 
             //o = variable
             if (isComplex) {
                 selectVariables.add("P" + column + " AS o");
                 explodedColumns.add(column);
+                //whereConditions.add("P" + column + " IS NOT NULL");
             } else {
                 selectVariables.add(column + " AS o");
                 whereConditions.add(column + " IS NOT NULL");
@@ -85,16 +89,38 @@ public class GraphPath {
             query.append(" WHERE ");
             query.append(String.join(" AND ", whereConditions));
 
-            logger.info(query);
+            //logger.info(query);
 
             Dataset<Row> data = sqlContext.sql(query.toString());
             if (result == null) {
                 result = data;
-            } else {
+            } else{
                 result = result.union(data);
             }
         }
         logger.info("COMPUTED HOPS FROM " + source);
         return result;
+    }
+
+    private static Dataset<Row> computeSources(final SQLContext sqlContext) {
+        String query = "SELECT DISTINCT s FROM wide_property_table";
+        Dataset<Row> data = sqlContext.sql(query);
+        return data;
+    }
+
+    private static void partitionTable(final SQLContext sqlContext, String originalTableName, String partitionedTableName, String columnName){
+        Dataset<Row> data = sqlContext.sql("Select * FROM " + originalTableName);
+        logger.info("saving partitioned table");
+        data.write().mode(SaveMode.Overwrite).partitionBy(columnName)
+                .format("parquet").saveAsTable(partitionedTableName);
+        logger.info("DONE!");
+    }
+
+    private static void repartitionTable(final SQLContext sqlContext, String originalTableName, String partitionedTableName, String columnName){
+        Dataset<Row> data = sqlContext.sql("Select * FROM " + originalTableName);
+        logger.info("saving partitioned table");
+        data = data.repartition(data.col(columnName));
+        data.write().mode(SaveMode.Overwrite).format("parquet").saveAsTable(partitionedTableName);
+        logger.info("DONE!");
     }
 }
