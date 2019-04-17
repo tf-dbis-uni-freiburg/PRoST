c7b3f2c7856f2d5e8057ea3f104b576dcdf09c21
gschievelbein <gschievelbein@inf.ufrgs.br>
2019-04-17 18:31:49 +0200

    grouped nodes use their characteristic set cardinality estimation

--- query-executor/src/main/java/joinTree/MVNode.java
+++ query-executor/src/main/java/joinTree/MVNode.java
@@ -53,7 +53,7 @@ public abstract class MVNode extends Node {
 	"Characteristic sets: Accurate cardinality estimation for RDF queries with multiple joins."
 	2011 IEEE 27th International Conference on Data Engineering. IEEE, 2011.
 	 */
-	public long computeStarJoinCardinality() {
+	private long computeStarJoinCardinality() {
 		final ArrayList<CharacteristicSetStatistics> superSets = computeCharacteristicSupersets(
 				this.computeCharacteristicSet(), statistics);
 		long cardinality = 0;
@@ -73,5 +73,9 @@ public abstract class MVNode extends Node {
 			cardinality = cardinality + superSet.getDistinctSubjects() * m * o;
 		}
 		return cardinality;
+	}
+
+	public float heuristicNodePriority() {
+		return computeStarJoinCardinality();
 	}
 }
