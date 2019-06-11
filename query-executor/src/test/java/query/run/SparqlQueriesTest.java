4dbfebea21dc7225557d0e1ef68134caed0e4f72
gschievelbein <gschievelbein@inf.ufrgs.br>
2019-06-11 16:57:24 +0200

    partial data loading and node execution tests

--- query-executor/src/test/java/query/run/SparqlQueriesTest.java
+++ query-executor/src/test/java/query/run/SparqlQueriesTest.java
@@ -2,6 +2,7 @@ package query.run;
 
 import java.io.File;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.holdenkarau.spark.testing.JavaDataFrameSuiteBase;
@@ -9,10 +10,13 @@ import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoder;
 import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
 import org.junit.Test;
 import query.utilities.HiveDatabaseUtilities;
 import query.utilities.TestData;
 import query.utilities.TripleBean;
+import stats.DatabaseStatistics;
+import utils.Settings;
 
 /**
  * This class tests represents the highest level of testing, i.e. given a query
@@ -32,7 +36,6 @@ public class SparqlQueriesTest extends JavaDataFrameSuiteBase implements Seriali
 	 * when parsing the file.
 	 *
 	 */
-	@Test
 	public void queryOnSingleTriple() {
 		final List<TripleBean> triplesList = TestData.createSingleTripleTestData();
 		final Dataset<TripleBean> data = spark().createDataset(triplesList, triplesEncoder);
@@ -54,5 +57,58 @@ public class SparqlQueriesTest extends JavaDataFrameSuiteBase implements Seriali
 		final Executor executor = new Executor("singleTripleDb");
 		executor.setOutputFile(System.getProperty("user.dir") + "\\target\\test_output\\result");
 		executor.execute(jt);*/
+	}
+
+	@Test
+	public void queryOnTT() {
+		spark().sql("DROP DATABASE IF EXISTS ttQueryTest_db CASCADE");
+		spark().sql("CREATE DATABASE IF NOT EXISTS  ttQueryTest_db");
+		spark().sql("USE ttQueryTest_db");
+
+		// creates test tt table
+		final TripleBean t1 = new TripleBean();
+		t1.setS("<http://example.org/resource/Res1>");
+		t1.setP("<http://example.org/property/pro1>");
+		t1.setO("<http://example.org/resource/Res:1000>");
+
+		final TripleBean t2 = new TripleBean();
+		t2.setS("<http://example.org/resource/Res1>");
+		t2.setP("<http://example.org/property/pro1>");
+		t2.setO("<http://example.org/resource/Res3>");
+
+		final TripleBean t3 = new TripleBean();
+		t3.setS("<http://example.org/resource/Res3>");
+		t3.setP("<http://example.org/property/pro3>");
+		t3.setO("\"wow\\\" \\\" . \\\"ok\\\" hi\"");
+
+		final TripleBean t4 = new TripleBean();
+		t4.setS("<http://example.org/resource/Res4>");
+		t4.setP("<http://example.org/property/pro2>");
+		t4.setO("<http://example.org/resource/Res4>");
+
+		final TripleBean t5 = new TripleBean();
+		t5.setS("<http://example.org/resource/Res2>");
+		t5.setP("<http://example.org/property/pro3>");
+		t5.setO("\"wow hi\"");
+
+		final ArrayList<TripleBean> triplesList = new ArrayList<>();
+		triplesList.add(t1);
+		triplesList.add(t2);
+		triplesList.add(t3);
+		triplesList.add(t4);
+		triplesList.add(t5);
+
+		final Dataset<Row> ttDataset = spark().createDataset(triplesList, triplesEncoder).select("s", "p", "o").orderBy(
+				"s", "p","o");
+		ttDataset.write().saveAsTable("tripletable");
+
+
+		final Settings settings = new Settings.Builder("ttQueryTest_db").usingTTNodes().build();
+		final DatabaseStatistics statistics = new DatabaseStatistics("ttQueryTest_db");
+
+		/*final Triple triple = new Triple();
+		final TriplePattern pattern = new TriplePattern();
+		final TTNode ttNode = new TTNode();*/
+
 	}
 }
