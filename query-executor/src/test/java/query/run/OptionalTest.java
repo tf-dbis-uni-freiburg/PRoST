794623930bbd85f08e352651b799997bc7e6ca14
biedenka <biedenka@informatik.uni-freiburg.de>
2019-07-30 15:45:33 +0200

    Added Optional Sparql Tests.

--- query-executor/src/test/java/query/run/OptionalTest.java
+++ query-executor/src/test/java/query/run/OptionalTest.java
@@ -2,6 +2,7 @@ package query.run;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.holdenkarau.spark.testing.JavaDataFrameSuiteBase;
 import joinTree.JoinTree;
@@ -13,7 +14,13 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoder;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import org.junit.Test;
+import org.spark_project.guava.collect.ImmutableList;
+
 import query.utilities.TripleBean;
 import stats.DatabaseStatistics;
 import translator.Translator;
@@ -33,117 +40,225 @@ public class OptionalTest extends JavaDataFrameSuiteBase implements Serializable
 
 	@Test
 	public void queryTest() {
-		final DatabaseStatistics statistics = new DatabaseStatistics("queryTest_db");
+		final DatabaseStatistics statistics = new DatabaseStatistics("queryTest05_db");
 		Dataset<Row> fullDataset = initializeDb(statistics);
 		fullDataset = fullDataset.orderBy("s", "p", "o");
-		queryOnTT(statistics, fullDataset);
+//		queryOnTT(statistics, fullDataset);
 		queryOnVp(statistics, fullDataset);
-		queryOnWpt(statistics, fullDataset);
-		queryOnIwpt(statistics, fullDataset);
-		queryOnJwptOuter(statistics, fullDataset);
-		queryOnJwptLeftOuter(statistics, fullDataset);
+//		queryOnWpt(statistics, fullDataset);
+//		queryOnIwpt(statistics, fullDataset);
+//		queryOnJwptOuter(statistics, fullDataset);
+//		queryOnJwptLeftOuter(statistics, fullDataset);
 	}
-
+	
+	  
 	private void queryOnTT(final DatabaseStatistics statistics, final Dataset<Row> fullDataset) {
-		final Settings settings = new Settings.Builder("queryTest_db").usingTTNodes().usingCharacteristicSets().build();
+		final Settings settings = new Settings.Builder("queryTest05_db").usingTTNodes().usingCharacteristicSets().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		final Translator translator = new Translator(settings, statistics,
-				classLoader.getResource("queryTest.q").getPath());
+				classLoader.getResource("queryTest5.q").getPath());
 		final JoinTree joinTree = translator.translateQuery();
-		final Dataset<Row> result = joinTree.compute(spark().sqlContext()).orderBy("s", "p", "o");
-		assertDataFrameEquals(result, fullDataset);
-	}
+		
 
+		//EXPECTED
+		StructType schema = DataTypes.createStructType(new StructField[]{
+				DataTypes.createStructField("title", DataTypes.StringType, true),
+				DataTypes.createStructField("genre", DataTypes.StringType, true),
+				});
+		Row row1 = RowFactory.create("Title1", "Science");
+		Row row2 = RowFactory.create("Title2", null);
+		List<Row> rowList = ImmutableList.of(row1, row2);
+		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		
+		//ACTUAL
+		final Dataset<Row> actualResult = joinTree.compute(spark().sqlContext()).orderBy("title", "genre");
+		final Dataset<Row> nullableActualResult = sqlContext().createDataFrame(actualResult.collectAsList(),
+				actualResult.schema().asNullable());
+		
+		assertDataFrameEquals(expectedResult, nullableActualResult);
+				
+	}
+	
 	private void queryOnVp(final DatabaseStatistics statistics, final Dataset<Row> fullDataset) {
-		final Settings settings = new Settings.Builder("queryTest_db").usingVPNodes().build();
+		final Settings settings = new Settings.Builder("queryTest05_db").usingVPNodes().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		final Translator translator = new Translator(settings, statistics,
-				classLoader.getResource("queryTest.q").getPath());
+				classLoader.getResource("queryTest5.q").getPath());
 		final JoinTree joinTree = translator.translateQuery();
-		final Dataset<Row> result = joinTree.compute(spark().sqlContext()).orderBy("s", "p", "o");
-		final Dataset<Row> nullableResult = sqlContext().createDataFrame(result.collectAsList(),
-				result.schema().asNullable());
-		assertDataFrameEquals(nullableResult, fullDataset);
+		
+		//EXPECTED
+		StructType schema = DataTypes.createStructType(new StructField[]{
+				DataTypes.createStructField("title", DataTypes.StringType, true),
+				DataTypes.createStructField("genre", DataTypes.StringType, true),
+				});
+		Row row1 = RowFactory.create("Title1", "Science");
+		Row row2 = RowFactory.create("Title2", null);
+		List<Row> rowList = ImmutableList.of(row1, row2);
+		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		expectedResult.printSchema();
+		expectedResult.show();
+		
+		//ACTUAL
+		final Dataset<Row> actualResult = joinTree.compute(spark().sqlContext()).orderBy("title", "genre");
+		final Dataset<Row> nullableActualResult = sqlContext().createDataFrame(actualResult.collectAsList(),
+				actualResult.schema().asNullable());
+				
+		nullableActualResult.printSchema();
+		nullableActualResult.show();
+		
+		assertDataFrameEquals(expectedResult, nullableActualResult);
 	}
 
 	private void queryOnWpt(final DatabaseStatistics statistics, final Dataset<Row> fullDataset) {
-		final Settings settings = new Settings.Builder("queryTest_db").usingWPTNodes().build();
+		final Settings settings = new Settings.Builder("queryTest05_db").usingWPTNodes().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		final Translator translator = new Translator(settings, statistics,
-				classLoader.getResource("queryTest.q").getPath());
+				classLoader.getResource("queryTest5.q").getPath());
 		final JoinTree joinTree = translator.translateQuery();
-		final Dataset<Row> result = joinTree.compute(spark().sqlContext()).orderBy("s", "p", "o");
-		final Dataset<Row> nullableResult = sqlContext().createDataFrame(result.collectAsList(),
-				result.schema().asNullable());
-		assertDataFrameEquals(nullableResult, fullDataset);
+		
+		//EXPECTED
+		StructType schema = DataTypes.createStructType(new StructField[]{
+				DataTypes.createStructField("title", DataTypes.StringType, true),
+				DataTypes.createStructField("genre", DataTypes.StringType, true),
+				});
+		Row row1 = RowFactory.create("Title1", "Science");
+		Row row2 = RowFactory.create("Title2", null);
+		List<Row> rowList = ImmutableList.of(row1, row2);
+		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		
+		//ACTUAL
+		final Dataset<Row> actualResult = joinTree.compute(spark().sqlContext()).orderBy("title", "genre");
+		final Dataset<Row> nullableActualResult = sqlContext().createDataFrame(actualResult.collectAsList(),
+				actualResult.schema().asNullable());
+		
+		assertDataFrameEquals(expectedResult, nullableActualResult);
+		
 	}
 
 	private void queryOnIwpt(final DatabaseStatistics statistics, final Dataset<Row> fullDataset) {
-		final Settings settings = new Settings.Builder("queryTest_db").usingIWPTNodes().build();
+		final Settings settings = new Settings.Builder("queryTest05_db").usingIWPTNodes().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		final Translator translator = new Translator(settings, statistics,
-				classLoader.getResource("queryTest.q").getPath());
+				classLoader.getResource("queryTest5.q").getPath());
 		final JoinTree joinTree = translator.translateQuery();
-		final Dataset<Row> result = joinTree.compute(spark().sqlContext()).orderBy("s", "p", "o");
-		final Dataset<Row> nullableResult = sqlContext().createDataFrame(result.collectAsList(),
-				result.schema().asNullable());
-		assertDataFrameEquals(nullableResult, fullDataset);
+
+		//EXPECTED
+		StructType schema = DataTypes.createStructType(new StructField[]{
+				DataTypes.createStructField("title", DataTypes.StringType, true),
+				DataTypes.createStructField("genre", DataTypes.StringType, true),
+				});
+		Row row1 = RowFactory.create("Title1", "Science");
+		Row row2 = RowFactory.create("Title2", null);
+		List<Row> rowList = ImmutableList.of(row1, row2);
+		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		
+		//ACTUAL
+		final Dataset<Row> actualResult = joinTree.compute(spark().sqlContext()).orderBy("title", "genre");
+		final Dataset<Row> nullableActualResult = sqlContext().createDataFrame(actualResult.collectAsList(),
+				actualResult.schema().asNullable());
+		
+		assertDataFrameEquals(expectedResult, nullableActualResult);
+		
 	}
 
 	private void queryOnJwptOuter(final DatabaseStatistics statistics, final Dataset<Row> fullDataset) {
-		final Settings settings = new Settings.Builder("queryTest_db").usingJWPTOuterNodes().build();
+		final Settings settings = new Settings.Builder("queryTest05_db").usingJWPTOuterNodes().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		final Translator translator = new Translator(settings, statistics,
-				classLoader.getResource("queryTest.q").getPath());
+				classLoader.getResource("queryTest5.q").getPath());
 		final JoinTree joinTree = translator.translateQuery();
-		final Dataset<Row> result = joinTree.compute(spark().sqlContext()).orderBy("s", "p", "o");
-		final Dataset<Row> nullableResult = sqlContext().createDataFrame(result.collectAsList(),
-				result.schema().asNullable());
-		assertDataFrameEquals(nullableResult, fullDataset);
+		
+		//EXPECTED
+		StructType schema = DataTypes.createStructType(new StructField[]{
+				DataTypes.createStructField("title", DataTypes.StringType, true),
+				DataTypes.createStructField("genre", DataTypes.StringType, true),
+				});
+		Row row1 = RowFactory.create("Title1", "Science");
+		Row row2 = RowFactory.create("Title2", null);
+		List<Row> rowList = ImmutableList.of(row1, row2);
+		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		
+		//ACTUAL
+		final Dataset<Row> actualResult = joinTree.compute(spark().sqlContext()).orderBy("title", "genre");
+		final Dataset<Row> nullableActualResult = sqlContext().createDataFrame(actualResult.collectAsList(),
+				actualResult.schema().asNullable());
+		
+		assertDataFrameEquals(expectedResult, nullableActualResult);
+		
 	}
 
 	private void queryOnJwptLeftOuter(final DatabaseStatistics statistics, final Dataset<Row> fullDataset) {
-		final Settings settings = new Settings.Builder("queryTest_db").usingJWPTLeftouterNodes().build();
+		final Settings settings = new Settings.Builder("queryTest05_db").usingJWPTLeftouterNodes().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		final Translator translator = new Translator(settings, statistics,
-				classLoader.getResource("queryTest.q").getPath());
+				classLoader.getResource("queryTest5.q").getPath());
 		final JoinTree joinTree = translator.translateQuery();
-		final Dataset<Row> result = joinTree.compute(spark().sqlContext()).orderBy("s", "p", "o");
-		final Dataset<Row> nullableResult = sqlContext().createDataFrame(result.collectAsList(),
-				result.schema().asNullable());
-		assertDataFrameEquals(nullableResult, fullDataset);
+		
+		//EXPECTED
+		StructType schema = DataTypes.createStructType(new StructField[]{
+				DataTypes.createStructField("title", DataTypes.StringType, true),
+				DataTypes.createStructField("genre", DataTypes.StringType, true),
+				});
+		Row row1 = RowFactory.create("Title1", "Science");
+		Row row2 = RowFactory.create("Title2", null);
+		List<Row> rowList = ImmutableList.of(row1, row2);
+		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		
+		//ACTUAL
+		final Dataset<Row> actualResult = joinTree.compute(spark().sqlContext()).orderBy("title", "genre");
+		final Dataset<Row> nullableActualResult = sqlContext().createDataFrame(actualResult.collectAsList(),
+				actualResult.schema().asNullable());
+		
+		assertDataFrameEquals(expectedResult, nullableActualResult);
+		
 	}
 
 	private Dataset<Row> initializeDb(final DatabaseStatistics statistics) {
-		spark().sql("DROP DATABASE IF EXISTS queryTest_db CASCADE");
-		spark().sql("CREATE DATABASE IF NOT EXISTS  queryTest_db");
-		spark().sql("USE queryTest_db");
+		spark().sql("DROP DATABASE IF EXISTS queryTest05_db CASCADE");
+		spark().sql("CREATE DATABASE IF NOT EXISTS  queryTest05_db");
+		spark().sql("USE queryTest05_db");
 
+				
 		// creates test tt table
 		final TripleBean t1 = new TripleBean();
-		t1.setS("<http://example.org/resource/Res1>");
-		t1.setP("<http://example.org/property/pro1>");
-		t1.setO("<http://example.org/resource/Res:1000>");
+		t1.setS("<http://example.org/book1>");
+		t1.setP("<http://example.org/publishedBy>");
+		t1.setO("<http://springer.com/publisher>");
 
 		final TripleBean t2 = new TripleBean();
-		t2.setS("<http://example.org/resource/Res1>");
-		t2.setP("<http://example.org/property/pro1>");
-		t2.setO("<http://example.org/resource/Res3>");
+		t2.setS("<http://example.org/book1>");
+		t2.setP("<http://example.org/title>");
+		t2.setO("Title1");
 
 		final TripleBean t3 = new TripleBean();
-		t3.setS("<http://example.org/resource/Res3>");
-		t3.setP("<http://example.org/property/pro3>");
-		t3.setO("\"wow\\\" \\\" . \\\"ok\\\" hi\"");
+		t3.setS("<http://example.org/book1>");
+		t3.setP("<http://example.org/genre>");
+		t3.setO("Science");
 
 		final TripleBean t4 = new TripleBean();
-		t4.setS("<http://example.org/resource/Res4>");
-		t4.setP("<http://example.org/property/pro2>");
-		t4.setO("<http://example.org/resource/Res4>");
+		t4.setS("<http://example.org/book1>");
+		t4.setP("<http://example.org/writtenBy>");
+		t4.setO("<http://author1.com/author>");
 
 		final TripleBean t5 = new TripleBean();
-		t5.setS("<http://example.org/resource/Res2>");
-		t5.setP("<http://example.org/property/pro3>");
-		t5.setO("\"wow hi\"");
+		t5.setS("<http://author1.com/author>");
+		t5.setP("<http://example.org/name>");
+		t5.setO("Author1");
+		
+		final TripleBean t6 = new TripleBean();
+		t6.setS("<http://springer.com/publisher>");
+		t6.setP("<http://example.org/name>");
+		t6.setO("Springer-Verlag");
+		
+		final TripleBean t7 = new TripleBean();
+		t7.setS("<http://example.org/book2>");
+		t7.setP("<http://example.org/publishedBy>");
+		t7.setO("<http://springer.com/publisher>");
+
+		final TripleBean t8 = new TripleBean();
+		t8.setS("<http://example.org/book2>");
+		t8.setP("<http://example.org/title>");
+		t8.setO("Title2");
 
 		final ArrayList<TripleBean> triplesList = new ArrayList<>();
 		triplesList.add(t1);
@@ -151,14 +266,17 @@ public class OptionalTest extends JavaDataFrameSuiteBase implements Serializable
 		triplesList.add(t3);
 		triplesList.add(t4);
 		triplesList.add(t5);
+		triplesList.add(t6);
+		triplesList.add(t7);
+		triplesList.add(t8);
 
 		final Dataset<Row> ttDataset = spark().createDataset(triplesList, triplesEncoder).select("s", "p", "o").orderBy(
 				"s", "p", "o");
 		ttDataset.write().saveAsTable("tripletable");
 
 		final loader.Settings loaderSettings =
-				new loader.Settings.Builder("queryTest_db").withInputPath((System.getProperty(
-						"user.dir") + "\\target\\test_output\\sparqlQueriesTest").replace('\\', '/'))
+				new loader.Settings.Builder("queryTest05_db").withInputPath((System.getProperty(
+						"user.dir") + "\\target\\test_output\\OptionalTest").replace('\\', '/'))
 						.generateVp().generateWpt().generateIwpt().generateJwptOuter()
 						.generateJwptLeftOuter().generateJwptInner().build();
 
@@ -190,3 +308,34 @@ public class OptionalTest extends JavaDataFrameSuiteBase implements Serializable
 		return ttDataset;
 	}
 }
+
+/*
+TABLE:
+================================================================================================================
+"<http://example.org/book1>"		| "<http://example.org/publishedBy>"	| "<http://springer.com/publisher>"
+"<http://example.org/book1>"		| "<http://example.org/title>"			| "Title1"
+"<http://example.org/book1>"		| "<http://example.org/genre>"			| "Science"
+"<http://example.org/book1>"		| "<http://example.org/writtenBy>"		| "<http://author1.com/author>"
+
+"<http://author1.com/author>"		| "<http://example.org/name>"			| "Author1"
+"<http://springer.com/publisher>"	| "<http://example.org/name>"			| "Springer-Verlag"
+
+"<http://example.org/book2>"		| "<http://example.org/publishedBy>"	| "<http://springer.com/publisher>"
+"<http://example.org/book2>"		| "<http://example.org/title>"			| "Title2"
+================================================================================================================
+
+QUERY:
+-----------------------------------------------------------------------------------------------------------------
+SELECT ?title ?genre
+WHERE
+{
+	?book <http://example.org/title> ?title.
+	OPTIONAL {?book <http://example.org/genre> ?genre}
+}
+-----------------------------------------------------------------------------------------------------------------
+
+RESULT:
+-----------------------------------------------------------------------------------------------------------------
+?
+-----------------------------------------------------------------------------------------------------------------
+*/
