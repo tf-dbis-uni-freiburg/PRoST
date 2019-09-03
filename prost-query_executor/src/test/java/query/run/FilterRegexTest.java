e16d4ac306546fe89e878bfc7be040945d789fd9
gschievelbein <gschievelbein@inf.ufrgs.br>
2019-09-03 12:15:20 +0200

    Fixed code style issues. Removed sorting of unused dataset.

--- prost-query_executor/src/test/java/query/run/FilterRegexTest.java
+++ prost-query_executor/src/test/java/query/run/FilterRegexTest.java
@@ -17,7 +17,6 @@ import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.spark_project.guava.collect.ImmutableList;
 import query.utilities.TripleBean;
@@ -40,29 +39,27 @@ public class FilterRegexTest extends JavaDataFrameSuiteBase implements Serializa
 	@Test
 	public void queryTest2() throws Exception {
 		final DatabaseStatistics statistics = new DatabaseStatistics("queryTestFilterRegex1_db");
-		Dataset<Row> fullDataset = initializeDb2(statistics);
-		fullDataset = fullDataset.orderBy("s", "p", "o");
-		queryOnTT2(statistics, fullDataset);
-		queryOnVp2(statistics, fullDataset);
-		queryOnWpt2(statistics, fullDataset);
-		queryOnIwpt2(statistics, fullDataset);
-		queryOnJwptOuter2(statistics, fullDataset);
-		queryOnJwptLeftOuter2(statistics, fullDataset);
+		initializeDb2(statistics);
+		queryOnTT2(statistics);
+		queryOnVp2(statistics);
+		queryOnWpt2(statistics);
+		queryOnIwpt2(statistics);
+		queryOnJwptOuter2(statistics);
+		queryOnJwptLeftOuter2(statistics);
 	}	
-	private void queryOnTT2(final DatabaseStatistics statistics, final Dataset<Row> fullDataset)  throws Exception {
-		final Settings settings = new Settings.Builder("queryTestFilterRegex1_db").usingTTNodes().usingCharacteristicSets().build();
+	private void queryOnTT2(final DatabaseStatistics statistics)  throws Exception {
+		final Settings settings = new Settings.Builder("queryTestFilterRegex1_db").usingTTNodes().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		
 		final Query query = new Query(classLoader.getResource("queryTestFilterRegex1.q").getPath(), statistics, settings);
-		
 
 		//EXPECTED
-		StructType schema = DataTypes.createStructType(new StructField[]{
+		final StructType schema = DataTypes.createStructType(new StructField[]{
 				DataTypes.createStructField("x", DataTypes.StringType, true),
 				});
-		Row row1 = RowFactory.create("<http://example.org>", "string containing example");
-		List<Row> rowList = ImmutableList.of(row1);
-		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		final Row row1 = RowFactory.create("<http://example.org>", "string containing example");
+		final List<Row> rowList = ImmutableList.of(row1);
+		final Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
 
 		//ACTUAL
 		final Dataset<Row> actualResult = query.compute(spark().sqlContext()).orderBy("x");
@@ -77,20 +74,19 @@ public class FilterRegexTest extends JavaDataFrameSuiteBase implements Serializa
 		assertDataFrameEquals(expectedResult, nullableActualResult);
 	}
 	
-	private void queryOnVp2(final DatabaseStatistics statistics, final Dataset<Row> fullDataset)  throws Exception {
+	private void queryOnVp2(final DatabaseStatistics statistics)  throws Exception {
 		final Settings settings = new Settings.Builder("queryTestFilterRegex1_db").usingVPNodes().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		
 		final Query query = new Query(classLoader.getResource("queryTestFilterRegex1.q").getPath(), statistics, settings);
-		
-		
+
 		//EXPECTED
-		StructType schema = DataTypes.createStructType(new StructField[]{
+		final StructType schema = DataTypes.createStructType(new StructField[]{
 				DataTypes.createStructField("x", DataTypes.StringType, true),
 				});
-		Row row1 = RowFactory.create("<http://example.org>", "string containing example");
-		List<Row> rowList = ImmutableList.of(row1);
-		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		final Row row1 = RowFactory.create("<http://example.org>", "string containing example");
+		final List<Row> rowList = ImmutableList.of(row1);
+		final Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
 
 		//ACTUAL
 		final Dataset<Row> actualResult = query.compute(spark().sqlContext()).orderBy("x");
@@ -100,44 +96,41 @@ public class FilterRegexTest extends JavaDataFrameSuiteBase implements Serializa
 		assertDataFrameEquals(expectedResult, nullableActualResult);
 	}
 
-	private void queryOnWpt2(final DatabaseStatistics statistics, final Dataset<Row> fullDataset)  throws Exception {
+	private void queryOnWpt2(final DatabaseStatistics statistics)  throws Exception {
 		final Settings settings = new Settings.Builder("queryTestFilterRegex1_db").usingWPTNodes().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		
 		final Query query = new Query(classLoader.getResource("queryTestFilterRegex1.q").getPath(), statistics, settings);
-		
-		
+
 		//EXPECTED
-		StructType schema = DataTypes.createStructType(new StructField[]{
+		final StructType schema = DataTypes.createStructType(new StructField[]{
 				DataTypes.createStructField("x", DataTypes.StringType, true),
 				});
-		Row row1 = RowFactory.create("<http://example.org>", "string containing example");
-		List<Row> rowList = ImmutableList.of(row1);
-		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		final Row row1 = RowFactory.create("<http://example.org>", "string containing example");
+		final List<Row> rowList = ImmutableList.of(row1);
+		final Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
 
 		//ACTUAL
 		final Dataset<Row> actualResult = query.compute(spark().sqlContext()).orderBy("x");
 		final Dataset<Row> nullableActualResult = sqlContext().createDataFrame(actualResult.collectAsList(),
 				actualResult.schema().asNullable());		
-		
-		
+
 		assertDataFrameEquals(expectedResult, nullableActualResult);
 	}
 
-	private void queryOnIwpt2(final DatabaseStatistics statistics, final Dataset<Row> fullDataset)  throws Exception {
+	private void queryOnIwpt2(final DatabaseStatistics statistics)  throws Exception {
 		final Settings settings = new Settings.Builder("queryTestFilterRegex1_db").usingIWPTNodes().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		
 		final Query query = new Query(classLoader.getResource("queryTestFilterRegex1.q").getPath(), statistics, settings);
-		
 
 		//EXPECTED
-		StructType schema = DataTypes.createStructType(new StructField[]{
+		final StructType schema = DataTypes.createStructType(new StructField[]{
 				DataTypes.createStructField("x", DataTypes.StringType, true),
 				});
-		Row row1 = RowFactory.create("<http://example.org>", "string containing example");
-		List<Row> rowList = ImmutableList.of(row1);
-		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		final Row row1 = RowFactory.create("<http://example.org>", "string containing example");
+		final List<Row> rowList = ImmutableList.of(row1);
+		final Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
 
 		//ACTUAL
 		final Dataset<Row> actualResult = query.compute(spark().sqlContext()).orderBy("x");
@@ -147,20 +140,19 @@ public class FilterRegexTest extends JavaDataFrameSuiteBase implements Serializa
 		assertDataFrameEquals(expectedResult, nullableActualResult);
 	}
 
-	private void queryOnJwptOuter2(final DatabaseStatistics statistics, final Dataset<Row> fullDataset)  throws Exception {
+	private void queryOnJwptOuter2(final DatabaseStatistics statistics)  throws Exception {
 		final Settings settings = new Settings.Builder("queryTestFilterRegex1_db").usingJWPTOuterNodes().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		
 		final Query query = new Query(classLoader.getResource("queryTestFilterRegex1.q").getPath(), statistics, settings);
-		
-		
+
 		//EXPECTED
-		StructType schema = DataTypes.createStructType(new StructField[]{
+		final StructType schema = DataTypes.createStructType(new StructField[]{
 				DataTypes.createStructField("x", DataTypes.StringType, true),
 				});
-		Row row1 = RowFactory.create("<http://example.org>", "string containing example");
-		List<Row> rowList = ImmutableList.of(row1);
-		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		final Row row1 = RowFactory.create("<http://example.org>", "string containing example");
+		final List<Row> rowList = ImmutableList.of(row1);
+		final Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
 
 		//ACTUAL
 		final Dataset<Row> actualResult = query.compute(spark().sqlContext()).orderBy("x");
@@ -170,20 +162,19 @@ public class FilterRegexTest extends JavaDataFrameSuiteBase implements Serializa
 		assertDataFrameEquals(expectedResult, nullableActualResult);
 	}
 
-	private void queryOnJwptLeftOuter2(final DatabaseStatistics statistics, final Dataset<Row> fullDataset)  throws Exception {
+	private void queryOnJwptLeftOuter2(final DatabaseStatistics statistics)  throws Exception {
 		final Settings settings = new Settings.Builder("queryTestFilterRegex1_db").usingJWPTLeftouterNodes().build();
 		final ClassLoader classLoader = getClass().getClassLoader();
 		
 		final Query query = new Query(classLoader.getResource("queryTestFilterRegex1.q").getPath(), statistics, settings);
 		
-		
 		//EXPECTED
-		StructType schema = DataTypes.createStructType(new StructField[]{
+		final StructType schema = DataTypes.createStructType(new StructField[]{
 				DataTypes.createStructField("x", DataTypes.StringType, true),
 				});
-		Row row1 = RowFactory.create("<http://example.org>", "string containing example");
-		List<Row> rowList = ImmutableList.of(row1);
-		Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
+		final Row row1 = RowFactory.create("<http://example.org>", "string containing example");
+		final List<Row> rowList = ImmutableList.of(row1);
+		final Dataset<Row> expectedResult = spark().createDataFrame(rowList, schema);
 
 		//ACTUAL
 		final Dataset<Row> actualResult = query.compute(spark().sqlContext()).orderBy("x");
@@ -236,7 +227,6 @@ public class FilterRegexTest extends JavaDataFrameSuiteBase implements Serializa
 
 		return ttDataset;
 	}
-	
 }
 
 /*
